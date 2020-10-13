86090e697add6aa5125c02a544cc8d4deb9caa64
maulika27 <68596398+maulika27@users.noreply.github.com>
2020-10-13 15:50:18 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,46 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.Visibilty = 255;
+      
+      
+      
+
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        
+        push ();
+        translate(pos.x, pos.y);
+        rotate(angle);
+
+        if(this.body.speed<3){
+         
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        
+        }
+    
+        else{
+          World.remove(world, this.body);
+         
+          
+          this.Visibility = this.Visibility-5;
+          tint (255, this.Visibility)
+          image(this.image, this.body.position.x, this.body.position.y);
+            }
+            pop ();  
+      }
+        
+      }
